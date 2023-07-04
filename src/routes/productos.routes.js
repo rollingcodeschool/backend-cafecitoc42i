062ad883cd916b7039f7286f0ad7569b43a6d117
Emilse Arias <emilsearias@gmail.com>
2023-07-04 18:14:12 -0300

    agregamos la validacion al campo precio e imagen con express validator

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -22,8 +22,27 @@ router
       check("nombreProducto")
         .notEmpty()
         .withMessage("El nombre del producto es obligatorio")
-        .isLength({min: 2, max: 100})
-        .withMessage('El nombre del producto debe tener entre 2 y 100 caracteres como maximo')
+        .isLength({ min: 2, max: 100 })
+        .withMessage(
+          "El nombre del producto debe tener entre 2 y 100 caracteres como maximo"
+        ),
+      check('precio')
+          .notEmpty()
+          .withMessage('El precio es un dato obligatorio')
+          .isNumeric()
+          .withMessage('El precio debe ser un número')
+          .custom(((value)=>{
+            if( value >= 1 && value<=10000){
+              return true;
+            }else{
+              throw new Error('El precio debe estar entre 1 y 10000')
+            }
+          })),
+      check('imagen')
+          .notEmpty()
+          .withMessage('La url de la imagen es obligatoria')
+          .matches(/(https?:\/\/.*\.(?:png|jpeg))/)
+          .withMessage('Debe ser una URL de imagen valida, con extension (png|jpe?g|gif|bmp) ')
     ],
     crearProducto
   );
